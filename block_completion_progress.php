72194f6aa477f959aecab89b9cb4fbeaa133ea18
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-04-04 13:18:46 +0100

    Pass json data to chart renderer

--- block_completion_progress.php
+++ block_completion_progress.php
@@ -279,6 +279,8 @@ class block_completion_progress extends block_base {
 
         // load the chart using Chart.js
         $this->page->requires->js('/blocks/completion_progress/thirdparty/Chart.js', true);
+        
+        // set up the data
         $labels = array("January", "February", "March", "April", "May", "June", "July");
         $data = array(65, 59, 80, 81, 56, 55, 40);
         
