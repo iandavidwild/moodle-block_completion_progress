08e2e43ce51187671f58664cfc086a8d9bab87c7
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-04-18 18:23:40 +0100

    Display a coloured pie chart with clickable segments for each activity.

--- amd/src/chart_renderer.js
+++ amd/src/chart_renderer.js
@@ -26,45 +26,109 @@
  * @since      3.1
  */
 
-define(['jquery'], function($) {    
-	var t = {
+define(['jquery'], function($) {  
 	
-        drawLineChart: function(dataset) {
-
-			// Add a helper to format timestamp data
-			Date.prototype.formatMMDDYYYY = function() {
-				return (this.getMonth() + 1) +
-				"/" +  this.getDate() +
-				"/" +  this.getFullYear();
-			}
-
+	var links = [];
+	
+	var t = {
+		
+        drawChart: function(dataset) {
+        	
 			// Create the chart.js data structure using 'labelset' and 'dataset'
 			var completion_data = $.parseJSON(dataset);
+			var array_len = completion_data.activities.length;
+			
+			var labels = [];
+			var data = [];
+			var background = [];
+			
+			// only handle complete and incomplete at the moment
+			var col_complete = completion_data.colors.completed_colour;
+			var col_incomplete = completion_data.colors.notCompleted_colour;
+			var col_submitted = completion_data.colors.submittednotcomplete_colour;
+			var col_failed = completion_data.colors.futureNotCompleted_colour;
+			
+			for (var i = 0; i < array_len; i++) {
+				labels.push(completion_data.activities[i].name);
+				// Note that the pie chart segments will all be the same size
+				data.push('1'); 
+				links.push(completion_data.activities[i].link);
+				var status = completion_data.activities[i].status;
+				switch (status) {
+					case 'complete':
+						background.push(col_complete);
+						break;
+					case 'passed':
+						background.push(col_complete);
+						break;
+					case 'failed':
+						background.push(col_failed);
+						break;
+					case 'submitted':
+						background.push(col_submitted);
+						break;
+					default:
+						background.push(col_incomplete);
+						break;
+				}
+			}
 			
 			var tempData = {
-			
-			
 				labels: labels,
 				datasets:[
 					{
-						label: "Total Bills",
-						fillColor: "rgba(220,220,220,0.2)",
-						strokeColor: "rgba(220,220,220,1)",
-						pointColor: "rgba(220,220,220,1)",
-						pointStrokeColor: "#fff",
-						pointHighlightFill: "#fff",
-						pointHighlightStroke: "rgba(220,220,220,1)",
-						data: data
+						label: "Completions",
+						data: data,
+						backgroundColor: background
 				    }
 				]
 			};
 
     	    // Get the context of the canvas element we want to select
-    	    var ctx = document.getElementById("myLineChart").getContext("2d");
+    	    var ctx = document.getElementById("myChart").getContext("2d");
 
     	    // Instantiate a new chart
-    	    var myLineChart = new Chart(ctx, {type: 'line', data: tempData});	
+    	    var myChart = new Chart(ctx, {
+				type: 'pie',
+				data: tempData,
+				options: {
+					legend: {
+						display: false
+					},
+				    tooltips: {
+			            callbacks: {
+			                label: function(tooltipItem, data) {
+			                	String.prototype.trunc = 
+			                	      function(n){
+			                	          return this.substr(0,n-1)+(this.length>n?'...':'');
+			                	      };
+			                	      
+			                    var allData = data.datasets[tooltipItem.datasetIndex].data;
+			                    var tooltipLabel = data.labels[tooltipItem.index];
+			                    // truncate the label to 15 characters plus an elipsis if necessary:
+			                    return tooltipLabel.trunc(15);
+			                }
+			            }
+			        }
+				}
+			});
+    	    
+    	    $(document).ready(
+    		  function () {
+    		    var canvas = document.getElementById("myChart");
+    		    
+    		    canvas.onclick = function (evt) {
+    		      var activePoints = myChart.getElementsAtEvent(evt);
+    		      var chartData = activePoints[0]['_chart'].config.data;
+    		      var idx = activePoints[0]['_index'];
+
+    		      var url = links[idx];
+    		      window.location.href = url;
+    		      return false;
+    		    };
+    		  });
         },
+        
 	};
 	
 	return t;
