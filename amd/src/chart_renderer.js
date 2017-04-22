a6944fa09bb8cdaa58e61b02ef0e5f4139b06c13
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-04-22 16:43:04 +0100

    Register new Chart.js function to render text in the centre of a doughnut chart.

--- amd/src/chart_renderer.js
+++ amd/src/chart_renderer.js
@@ -32,7 +32,7 @@ define(['jquery'], function($) {
 	
 	var t = {
 		
-        drawChart: function(chartEl, dataSet) {
+        drawChart: function(chartEl, chartType, dataSet, showText) {
         	
 			// Create the chart.js data structure using 'labelset' and 'dataset'
 			var completion_data = $.parseJSON(dataSet);
@@ -73,6 +73,48 @@ define(['jquery'], function($) {
 				}
 			}
 			
+			Chart.pluginService.register({
+				beforeDraw: function (chart) {
+					if (chart.config.options.elements.center) {
+				        //Get ctx from string
+				        var ctx = chart.chart.ctx;
+				        
+								//Get options from the center object in options
+				        var centerConfig = chart.config.options.elements.center;
+				      	var fontStyle = centerConfig.fontStyle || 'Arial';
+								var txt = centerConfig.text;
+				        var color = centerConfig.color || '#000';
+				        var sidePadding = centerConfig.sidePadding || 20;
+				        var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+				        //Start with a base font of 30px
+				        ctx.font = "30px " + fontStyle;
+				        
+								//Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+				        var stringWidth = ctx.measureText(txt).width;
+				        var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+		
+				        // Find out how much the font can grow in width.
+				        var widthRatio = elementWidth / stringWidth;
+				        var newFontSize = Math.floor(30 * widthRatio);
+				        var elementHeight = (chart.innerRadius * 2);
+		
+				        // Pick a new font size so it will not be larger than the height of label.
+				        var fontSizeToUse = Math.min(newFontSize, elementHeight);
+		
+						//Set font settings to draw it correctly.
+				        ctx.textAlign = 'center';
+				        ctx.textBaseline = 'middle';
+				        var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+				        var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+				        ctx.font = fontSizeToUse+"px " + fontStyle;
+				        ctx.fillStyle = color;
+				        
+				        //Draw text in center
+				        ctx.fillText(txt, centerX, centerY);
+					}
+				}
+			});
+			
 			var chartData = {
 				labels: labels,
 				datasets:[
@@ -87,11 +129,24 @@ define(['jquery'], function($) {
     	    // Get the context of the canvas element we want to select
     	    var ctx = document.getElementById(chartEl).getContext("2d");
 
+    	    var centerTxt = ''; 
+    	    if(showText == true) {
+    	    	centerTxt = completion_data.percentage + '%';
+    	    }
+    	    
     	    // Instantiate a new chart
     	    var myChart = new Chart(ctx, {
-				type: 'pie',
+				type: chartType,
 				data: chartData,
 				options: {
+					elements: {
+						center: {
+							text: centerTxt,
+							color: '#000000', // Default is #000000
+							fontStyle: 'Arial', // Default is Arial
+							sidePadding: 40 // Default is 20 (as a percentage)
+						}
+					},
 					legend: {
 						display: false
 					},
