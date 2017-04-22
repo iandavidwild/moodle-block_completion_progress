80d60a007770bcefe13cdba8e630848f59eda9af
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-04-22 22:35:44 +0100

    Implement new function to display a chart of activity status in 'stacked bar' form.

--- amd/src/chart_renderer.js
+++ amd/src/chart_renderer.js
@@ -33,8 +33,7 @@ define(['jquery'], function($) {
 	var t = {
 		
         drawChart: function(chartEl, chartType, dataSet, showText) {
-        	
-			// Create the chart.js data structure using 'labelset' and 'dataset'
+       
 			var completion_data = $.parseJSON(dataSet);
 			var array_len = completion_data.activities.length;
 			
@@ -183,7 +182,120 @@ define(['jquery'], function($) {
     		    };
     		});
         },
-        
+
+        drawBar: function(chartEl, dataSet) {
+       
+			var completion_data = $.parseJSON(dataSet);
+			var array_len = completion_data.activities.length;
+			
+			var labels = [];
+			var data = [];
+			var background = [];
+			
+			// only handle complete and incomplete at the moment
+			var col_complete = completion_data.colors.completed_colour;
+			var col_incomplete = completion_data.colors.notCompleted_colour;
+			var col_submitted = completion_data.colors.submittednotcomplete_colour;
+			var col_failed = completion_data.colors.futureNotCompleted_colour;
+			
+			for (var i = 0; i < array_len; i++) {
+				labels.push(completion_data.activities[i].name);
+				// Note that the pie chart segments will all be the same size
+				data.push('1'); 
+				links.push(completion_data.activities[i].link);
+				var status = completion_data.activities[i].status;
+				switch (status) {
+					case 'complete':
+						background.push(col_complete);
+						break;
+					case 'passed':
+						background.push(col_complete);
+						break;
+					case 'failed':
+						background.push(col_failed);
+						break;
+					case 'submitted':
+						background.push(col_submitted);
+						break;
+					default:
+						background.push(col_incomplete);
+						break;
+				}
+			}
+			
+			var chartData = {
+				labels: labels,
+				datasets:[
+					{
+						label: "Completions",
+						data: data,
+						backgroundColor: background
+				    }
+				]
+			};
+
+    	    // Get the context of the canvas element we want to select
+    	    var ctx = document.getElementById(chartEl).getContext("2d");
+    	    
+    	    Chart.defaults.global.tooltips.enabled = false;
+    	    
+    	    
+    	    // Instantiate a new chart
+    	    var myChart = new Chart(ctx, {
+				type: 'horizontalBar',
+				data: chartData,	
+				options: {
+					scales: {
+				        xAxes: [{
+				            ticks: {
+				                fontFamily: "'Open Sans Bold', sans-serif",
+				                fontSize:11,
+				                display: false,
+				            },
+				            scaleLabel:{
+				                display:false
+				            },
+				            gridLines: {
+				            	display:false
+				            }, 
+				            stacked: true
+				        }],
+				        yAxes: [{
+				            gridLines: {
+				                display:false,
+				                color: "#fff",
+				                zeroLineColor: "#fff",
+				                zeroLineWidth: 0
+				            },
+				            ticks: {
+				                fontFamily: "'Open Sans Bold', sans-serif",
+				                fontSize:11
+				            },
+				            stacked: true
+				        }]
+				    },
+				    legend:{
+				        display:false
+				    },
+				    
+				}
+			});
+    	    
+    	    $(document).ready(
+    		  function () {
+    		    var canvas = document.getElementById(chartEl);
+    		    
+    		    canvas.onclick = function (evt) {
+    		      var activePoints = myChart.getElementsAtEvent(evt);
+    		      var chartData = activePoints[0]['_chart'].config.data;
+    		      var idx = activePoints[0]['_index'];
+
+    		      var url = links[idx];
+    		      window.location.href = url;
+    		      return false;
+    		    };
+    		});
+        },
 	};
 	
 	return t;
